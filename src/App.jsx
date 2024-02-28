b1b52c1bc2b80d2cd35c3f17e4c095a81e6d2f62
segleston <suraheg@gmail.com>
2024-02-28 21:33:19 +0000

    updated Page to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer'
-import { Candidate, Custom404, Employer, Home, JobDetail, JobResults } from './components/Pages';
+import { Candidate, Custom404, Employer, Home, JobDetail, JobResults } from './components/pages';
 import './App.css'
 
 function App() {
