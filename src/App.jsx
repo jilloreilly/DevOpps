ab14dda4004786ea057f573821a963f2c3fd546d
segleston <suraheg@gmail.com>
2024-02-28 21:13:10 +0000

    added starter code to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer'
+import { Contact, Custom404, Home, ProjectGallery, ProjectDetails, Resume } from './components/Pages';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <Router >
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='candidate' element={<Candidate />} />
+        <Route path='employer' element={<Employer />} />
+        <Route path='404' element={<Custom404 />} />
+        <Route path="/job/:id" element={<JobDetails />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
