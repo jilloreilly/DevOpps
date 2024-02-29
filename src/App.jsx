e4bc37b715a99e1aa5ba75f4afcab4675033edb4
Ramon Andrade <124772095+ramonsaguini@users.noreply.github.com>
2024-02-29 20:43:29 +0000

    Added function Search + Candidates.Json

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Header from './components/Header';
 // import Footer from './components/Footer'
-import { Candidate, Custom404, Employer, Home, JobDetails, JobResults } from './pages';
+import { Candidate, Custom404, Home, JobDetails, JobResults, EmployerSearch } from './pages';
 import './App.css'
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='candidate' element={<Candidate />} />
-        <Route path='employer' element={<Employer />} />
+        <Route path='employer/search' element={<EmployerSearch />} />
         <Route path='404' element={<Custom404 />} />
         <Route path="/job/:id" element={<JobDetails />} />
       </Routes>
