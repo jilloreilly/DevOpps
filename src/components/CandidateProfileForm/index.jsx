710bb4d4df98f816b03a69846a158648d9279b8c
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-29 19:48:08 +0000

    added some input change and button click functions that track the users input in the git hub input field and console logs it

--- src/components/CandidateProfileForm/index.jsx
+++ src/components/CandidateProfileForm/index.jsx
@@ -1,6 +1,16 @@
-
+import { useRef, useState } from "react";
 
 function CandidateProfile() {
+
+  const [gitHubProfile, setGitHubProfile] = useState('')
+
+  const handleInputChange = (e) => {
+    setGitHubProfile(e.target.value)
+  }
+
+  const handleButtonClick = () => {
+    console.log('Input Value: ', gitHubProfile)
+  }
 
   return (
     <>
@@ -162,12 +172,14 @@ function CandidateProfile() {
                 id="git-hub-link"
                 name="git-hub-link"
                 type="text"
+            value={gitHubProfile}
+            onChange={handleInputChange}
                 autoComplete="email"
                 required
                 className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 placeholder="Enter your GitHub Account"
               />
-              <button
+              <button id ="fetch-git-hub" onClick={handleButtonClick}
                 type="submit"
                 className="flex-none rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
