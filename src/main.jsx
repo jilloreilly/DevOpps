29d8feacea9405f7ccd0e06ecd54571f101b3304
Ramon Andrade <124772095+ramonsaguini@users.noreply.github.com>
2024-02-29 18:56:19 +0000

    added functions + candidates.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {NextUIProvider} from '@nextui-org/react'
-import App from './App.jsx'
 import './index.css'
+import EmployerSearch from './components/EmployerSearch/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <EmployerSearch />
+      
       </NextUIProvider>  
   </React.StrictMode>,
 )
