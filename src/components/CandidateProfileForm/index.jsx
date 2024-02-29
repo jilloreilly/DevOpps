17b463d2ef2472240ed7ddb50d70cb8356a66e31
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-29 20:00:11 +0000

    added a fetch request to retrieve github profile

--- src/components/CandidateProfileForm/index.jsx
+++ src/components/CandidateProfileForm/index.jsx
@@ -4,12 +4,27 @@ function CandidateProfile() {
 
   const [gitHubProfile, setGitHubProfile] = useState('')
 
-  const handleInputChange = (e) => {
+  const handleGitHubChange = (e) => {
     setGitHubProfile(e.target.value)
   }
 
-  const handleButtonClick = () => {
+  const handleGitHubClick = () => {
     console.log('Input Value: ', gitHubProfile)
+    fetchGitHub(gitHubProfile)
+  }
+
+  const fetchGitHub = (username) => {
+
+    const queryURL = `https://api.github.com/users/${username}`
+    fetch(queryURL)
+      .then(response => {
+      return response.json()
+      })
+      .then(data => {
+      console.log(data)
+    })
+
+
   }
 
   return (
@@ -134,9 +149,6 @@ function CandidateProfile() {
               </div>
               
 
-
-
-
           </div>
         </div>
       </div>
@@ -173,13 +185,13 @@ function CandidateProfile() {
                 name="git-hub-link"
                 type="text"
             value={gitHubProfile}
-            onChange={handleInputChange}
+            onChange={handleGitHubChange}
                 autoComplete="email"
                 required
-                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3 py-2  shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 placeholder="Enter your GitHub Account"
               />
-              <button id ="fetch-git-hub" onClick={handleButtonClick}
+              <button id ="fetch-git-hub" onClick={handleGitHubClick}
                 type="submit"
                 className="flex-none rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
