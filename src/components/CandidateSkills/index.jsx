f1a54da5c0e0c8a1da04d39c08518d2828a4b31f
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-03-01 10:01:01 +0000

    updated skills checkboxes to include the skills listed by Ramon's employer search page

--- src/components/CandidateSkills/index.jsx
+++ src/components/CandidateSkills/index.jsx
@@ -7,21 +7,31 @@ export default function CandidateSkills () {
 
   return (
     <div className="flex flex-col gap-1 w-full">
+      <h2 class="text-3xl font-bold tracking-tight sm:text-3xl">Add your Skills</h2>
       <CheckboxGroup
-        className="gap-1"
-        label="Select amenities"
+        className="mt-5 gap-1"
         orientation="horizontal"
         value={groupSelected}
         onChange={setGroupSelected}
       >
-        <CustomCheckbox value="wifi">HTML5</CustomCheckbox>
-        <CustomCheckbox value="tv">CSS3</CustomCheckbox>
-        <CustomCheckbox value="kitchen">Javascript ES5/ES6</CustomCheckbox>
-        <CustomCheckbox value="parking">React</CustomCheckbox>
-        <CustomCheckbox value="pool">Bootstrap</CustomCheckbox>
-        <CustomCheckbox value="gym">Tailwindcss</CustomCheckbox>
+        <CustomCheckbox value="Javascript ES5/ES6">Javascript</CustomCheckbox>
+        <CustomCheckbox value="Python">Python</CustomCheckbox>
+        <CustomCheckbox value="Java">Java</CustomCheckbox>
+        <CustomCheckbox value="C#">C#</CustomCheckbox>
+        <CustomCheckbox value="PHP">PHP</CustomCheckbox>
+        <CustomCheckbox value="Ruby">Ruby</CustomCheckbox>
+        <CustomCheckbox value="Swift">Swift</CustomCheckbox>
+        <CustomCheckbox value="Swift">C++</CustomCheckbox>
+        <CustomCheckbox value="HTML/CSS">HTML/CSS</CustomCheckbox>
+        <CustomCheckbox value="SQL">SQL</CustomCheckbox>
+        <CustomCheckbox value="React">React</CustomCheckbox>
+        <CustomCheckbox value="Angular">React</CustomCheckbox>
+        <CustomCheckbox value="Vue.js">Vue.js</CustomCheckbox>
+        <CustomCheckbox value="Node.js">Node.js</CustomCheckbox>
+        <CustomCheckbox value="Bootstrap">Bootstrap</CustomCheckbox>
+        <CustomCheckbox value="Tailwind Css">Tailwind Css</CustomCheckbox>
       </CheckboxGroup>
-      <p className="mt-4 ml-1 text-default-500">
+      <p className="mt-4 mb-4 ml-1 text-default-500">
         Selected: {groupSelected.join(", ")}
       </p>
     </div>
