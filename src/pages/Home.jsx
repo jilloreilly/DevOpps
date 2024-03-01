55b5f93d4ca11ef9febda8638d6fc4217acdf487
segleston <segleston@gmail.com>
2024-03-01 19:47:56 +0000

    updated layout of jumbotron

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,22 +21,19 @@ function Home() {
 
   return (
     <>
-    <div className="h-screen flex items-center hero-background">
-      <section className="w-full bg-cover bg-center py-32">
-        <div className="container mx-auto text-center flex flex-col sm:flex-row justify-center items-center">
-          <h1 className="text-5xl font-medium mb-6 text-white">Welcome to DevOpps</h1>
-          <Link to="/job/search">
-            <Button variant="contained" color="primary" className="mb-4 sm:mb-0 sm:ml-4">Search Jobs</Button>
-          </Link>
-          <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row items-center sm:ml-4">
-            <input type="text" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} placeholder="Enter job title" className="mb-2 sm:mb-0 sm:mr-2 text-black" />
-            <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} placeholder="Enter location" className="mb-2 sm:mb-0 sm:mr-2 text-black" />
-            <Button type="submit" variant="contained" color="primary">Search</Button>
-          </form>
-          {errorMessage && <p className="text-white">{errorMessage}</p>}
-        </div>
-      </section>
-    </div>
+    <div className="h-screen flex flex-col items-center justify-center hero-background">
+      <div className="text-center mb-12">
+        <h1 className="text-5xl font-medium text-white">Welcome to DevOpps</h1>
+      </div>
+      <div className="container mx-auto text-center flex flex-col sm:flex-row justify-center items-center">
+        <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row items-center sm:ml-4">
+          <input type="text" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} placeholder="Enter job title" className="mb-2 sm:mb-0 sm:mr-2 text-black" />
+          <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} placeholder="Enter location" className="mb-2 sm:mb-0 sm:mr-2 text-black" />
+          <Button type="submit" variant="contained" color="primary">Search</Button>
+        </form>
+        {errorMessage && <p className="text-white">{errorMessage}</p>}
+      </div>
+      </div>
     <div className='container px-8 mx-auto'>
       <div className='grid grid-cols-1 sm:grid-cols-3 flex-row h-96'> 
       <div className='sm:col-span-2'>
