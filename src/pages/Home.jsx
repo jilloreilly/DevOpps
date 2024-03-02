927b86c3e7ef329c6193e80df1ae2b1b311ad54e
segleston <segleston@gmail.com>
2024-03-02 10:47:52 +0000

    amended home page, torubleshooting search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { Button } from "@nextui-org/react";
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import JobResults from './JobResults';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
   const navigate = useNavigate();
@@ -9,30 +8,33 @@ function Home() {
   const [location, setLocation] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
+  const handleSearch = (jobTitle, location) => {
+    navigate(`/job/results?title=${jobTitle}&location=${location}`);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     if (!jobTitle || !location) {
       setErrorMessage("Please fill in both job title and location.");
       return;
     }
-    handleSearch(jobTitle, location); // Call handleSearch with jobTitle and location
+    handleSearch(jobTitle, location);
   };
-  
 
   return (
     <>
-    <div className="h-screen flex flex-col items-center justify-center hero-background">
-      <div className="text-center mb-12">
-        <h1 className="text-5xl font-medium text-white">Welcome to DevOpps</h1>
-      </div>
-      <div className="container mx-auto text-center flex flex-col sm:flex-row justify-center items-center">
-        <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row items-center sm:ml-4">
-          <input type="text" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} placeholder="Enter job title" className="mb-2 sm:mb-0 sm:mr-2 text-black" />
-          <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} placeholder="Enter location" className="mb-2 sm:mb-0 sm:mr-2 text-black" />
-          <Button type="submit" variant="contained" color="primary">Search</Button>
-        </form>
-        {errorMessage && <p className="text-white">{errorMessage}</p>}
-      </div>
+      <div className="h-screen flex flex-col items-center justify-center hero-background">
+        <div className="text-center mb-12">
+          <h1 className="text-5xl font-medium text-white">Welcome to DevOpps</h1>
+        </div>
+        <div className="container mx-auto text-center flex flex-col sm:flex-row justify-center items-center">
+          <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row items-center sm:ml-4">
+            <input type="text" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} placeholder="Enter job title" className="mb-2 sm:mb-0 sm:mr-2 text-black" />
+            <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} placeholder="Enter location" className="mb-2 sm:mb-0 sm:mr-2 text-black" />
+            <Button type="submit" variant="contained" color="primary">Search</Button>
+          </form>
+          {errorMessage && <p className="text-white">{errorMessage}</p>}
+        </div>
       </div>
     <div className='container px-8 mx-auto'>
       <div className='grid grid-cols-1 sm:grid-cols-3 flex-row h-96'> 
