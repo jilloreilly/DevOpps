41a68c2566f5411af1d7c2d06a5bc55628781945
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-03-03 09:44:22 +0000

    Implement pagination

--- src/pages/JobSearch.jsx
+++ src/pages/JobSearch.jsx
@@ -7,6 +7,9 @@ function JobSearch() {
   // Job search results
   const [jobs, setJobs] = useState([]);
 
+  // Test for initial render
+  const [initialRender, setInitialRender] = useState(true);
+
   // Form data
   const [formData, setFormData] = useState({
     query: '',
@@ -21,20 +24,21 @@ function JobSearch() {
 
   // Listen for the page to change and rerun the search when it does
   useEffect(() => {
-    if (!isSearching) {
+    // But not if we are already searching
+    if (!initialRender && !isSearching) {
       handleJobSearch();
     }
+  }, [page, initialRender]);
+
+  useEffect(() => {
+    console.log("Page is set to " + page);
   }, [page]);
 
-  // Handle Next Page button
-  const handleNextPage = () => {
-    setPage(page + 1);
-  };
-
-  // Handle Previous Page button
-  const handlePrevPage = () => {
-    setPage(page - 1);
-  };
+  
+  // Set initialRender to false after first render
+  useEffect(() => {
+    setInitialRender(false);
+  }, []);
 
   // Handle form input changes
   const handleInputChange = (event) => {
@@ -51,39 +55,63 @@ function JobSearch() {
   };
 
   // Handle form submission
-  const handleJobSearch = async (event) => {
+  const handleJobSearch = async () => {
 
     setIsSearching(true);
 
-    // Prevent form submitting and set page to 0
-    if (event) {
-      event.preventDefault();
-      setPage(0);
+    // Get query and location from URL if provided
+    const queryString = new URLSearchParams(window.location.search);
+    const qsTitle = queryString.get('title');
+    const qsLocation = queryString.get('location');
+
+    let query;
+    let location;
+
+    if (!formData.query){
+      query = qsTitle;
+    }else{
+      query = formData.query;
+    }
+    
+    if (!formData.location){
+      location = qsLocation;
+    }else{
+      location = formData.location;
     }
 
-    let query = formData.query;
-    query = query.trim();
-    let location = formData.location;
-    location = location.trim();
+    // So long as we have a query and location, we can search
+    if (query && location) {
 
-    // Get jobs from API
-    const response = await getJobs({
-      query: query,
-      location: location,
-      index: page
-    });
+      // Get jobs from API
+      const response = await getJobs({
+        query: query,
+        location: location,
+        index: page
+      });
 
-    // Update jobs array
-    setJobs(response.data.jobs);
-    console.log(response);
+      // Update jobs array
+      setJobs(response.data.jobs);
+      console.log(response);
+    }
 
     setIsSearching(false);
+  };
+
+  // Handle form submission
+  const handleSearchFormSubmit = async (event) => {
+    event.preventDefault();
+    await handleJobSearch();
+  }
+
+  // Handle page change (eg +1 or -1)
+  const requestPageChange = (delta) => {
+    setPage(page + delta)
   };
 
   const JobSearchFilter = () => {
     return (
       <>
-        <form className="bg-white rounded-lg p-2 mr-2" onSubmit={handleJobSearch}>
+        <form onSubmit={handleSearchFormSubmit} className="bg-white rounded-lg p-2 mr-2">
           <div className="space-y-12">
             <div className="border-b border-gray-900/10 pb-6">
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -99,8 +127,7 @@ function JobSearch() {
                       type="text"
                       name="query"
                       placeholder="eg Frontend Developer"
-                      className="
-                    block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       required />
                   </div>
                 </div>
@@ -166,8 +193,7 @@ function JobSearch() {
             </button>
             <button
               type="submit"
-              onClick={handleJobSearch}
-              //disabled={isSearching}
+              disabled={isSearching}
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Find Jobs
             </button>
@@ -189,18 +215,18 @@ function JobSearch() {
         </aside>
 
         <section className="w-8/12 sm:w-full">          
-          <div id="next-page-nav" className={`flex justify-end ${jobs.length < 10 ? "hidden" : "block"}`}>
+          <div id="next-page-nav" className={`flex justify-end items-center mb-2 ${jobs.length < 10 ? "hidden" : "block"}`}>
             <button
               id="prev-button"
-              onClick={handlePrevPage}
+              onClick={() => requestPageChange(-1)}
               disabled={page < 1 || isSearching}
               className="rounded-md bg-indigo-600 px-3 py-2 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Previous
             </button>
-            <div className="inline-block mb-6">Page {page + 1}</div>
+            <div className="inline-block mx-2">Page {page + 1}</div>
             <button
               id="next-button"
-              onClick={handleNextPage}
+              onClick={() => requestPageChange(+1)}
               disabled={jobs.length < 10 || isSearching}
               className="rounded-md bg-indigo-600 px-3 py-2 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Next
