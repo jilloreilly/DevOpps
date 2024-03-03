6e6ac61623d5570827af5ffdeb1309b46682fa11
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-03-03 20:49:45 +0000

    updated app.jsx with candidate profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyHeader from './components/Header';
 import MyFooter from './components/Footer';
-import { Candidate, Custom404, Home, JobSearch, EmployerSearch, JobDetails } from './pages';
+import { CreateProfile, Custom404, Home, JobSearch, JobResults, EmployerSearch, JobDetails, Candidate } from './pages';
 import './App.css'
 import React from 'react';
+import userData from '../candidates.json'
 
 
 function App() {
@@ -13,9 +14,11 @@ function App() {
       <MyHeader />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/candidate' element={<Candidate />} />
-        <Route path='/job-results' element={<JobSearch />} />
+        <Route path='/candidate/create-profile' element={<CreateProfile />} />
+        <Route path="/candidate/profile/:id" element={<Candidate userData={userData} />} />
+        <Route path='/job-results' element={<JobResults />} />
         <Route path='/employer/search' element={<EmployerSearch />} />
+        <Route path="/job/search" element={<JobSearch />} />
         <Route path="/job-details/:id" element={<JobDetails />} />
         <Route path='*' element={<Custom404 />} />
       </Routes>
