94915b4d8d53461ca8aa1c4b85fad711265a30f7
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-03-03 20:51:37 +0000

    updated CandidateGitHubProfile folder with backup files

--- src/components/CandidateGitHubProfile/index.jsx
+++ src/components/CandidateGitHubProfile/index.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
-import { CompleteGitHubProfile } from "./CompleteGitHubProfile";
+import { GitHubSuccess } from "./GitHubSuccess";
+import { GitHubError } from "./GitHubError";
 
-function CandidateGitHubProfile() {
-
+function CandidateGitHubProfile({ onGitHubInputChange }) {
+  
   const [gitHubProfile, setGitHubProfile] = useState('')
   
-  const handleGitHubChange = (e) => {
+  const handleChange = (e) => {
     setGitHubProfile(e.target.value)
-  }
+    const { name, value } = e.target;
+    onGitHubInputChange(name, value);
+  };
+
 
   const handleGitHubClick = (e) => {
     e.preventDefault()
@@ -17,7 +21,7 @@ function CandidateGitHubProfile() {
   }
   
   const [gitHubDetails, setGitHubDetails] = useState({});
-  const [gitHubRepoDetails, setGitHubRepoDetails] = useState({});
+  const [gitHubRepoDetails, setGitHubRepoDetails] = useState([]);
   
   useEffect(() => {
     if (Object.keys(gitHubDetails).length > 0) {
@@ -25,6 +29,12 @@ function CandidateGitHubProfile() {
       console.log(gitHubDetails.repos_url)
     }
   }, [gitHubDetails]);
+
+  useEffect(() => {
+    if (Object.keys(gitHubRepoDetails).length > 0) {
+      console.log({ gitHubRepoDetails });
+    }
+  }, [gitHubRepoDetails]);
 
   const fetchGitHub = async (username) => {
     
@@ -53,11 +63,11 @@ function CandidateGitHubProfile() {
       console.log(response.data)
       setGitHubRepoDetails(response.data);
     } catch (error) {
+      alert('No GitHub Repo Found')
       console.error(error);
     }
     
 }
-
 
   const propsNotPassed = !gitHubDetails || !gitHubDetails.avatar_url || !gitHubDetails.repos_url || !gitHubDetails.followers;
 
@@ -78,10 +88,10 @@ function CandidateGitHubProfile() {
           
           <input
                 id="git-hub-link"
-                name="git-hub-link"
+                name="gitHubProfile"
                 type="text"
             value={gitHubProfile}
-            onChange={handleGitHubChange}
+            onChange={handleChange}
                 required
                 className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-0 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
                 placeholder="GitHub Username"
@@ -97,19 +107,26 @@ function CandidateGitHubProfile() {
       
 
       {propsNotPassed ? (
-        console.log('No Props Passed')
+        <div></div>
       ) : (
-          
-          <CompleteGitHubProfile
-            id={gitHubDetails.id}
-            url={gitHubDetails.html_url}
-            login={gitHubDetails.login}
-            avatar={gitHubDetails.avatar_url}
-            repos={gitHubDetails.repos_url}
-            followers={gitHubDetails.followers}
-            following={gitHubDetails.following}
-          />
-        
+          <>
+          <GitHubSuccess
+            // id={gitHubDetails.id}
+            // url={gitHubDetails.html_url}
+            // login={gitHubDetails.login}
+            // avatar={gitHubDetails.avatar_url}
+            // repos={gitHubDetails.repos_url}
+            // followers={gitHubDetails.followers}
+            // following={gitHubDetails.following}
+            />
+            <GitHubError />
+            {/* <h3 className="mt-5 font-bold sm:text-2xl">Repos</h3>
+            <ul className="mt-5 flex flex-wrap flex-row items-center justify-center gap-4">
+        {gitHubRepoDetails.map(repo => (
+          <button className="bg-blue-100 hover:bg-blue-200 text-blue-800 text-xs font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 inline-flex items-center justify-center" key={repo.id}>{repo.name}</button>
+        ))}
+      </ul> */}
+        </>
       )
       }
 
