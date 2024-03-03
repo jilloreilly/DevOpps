044abf733e2120c49be2fa799ec1f68c43da9d8d
Ramon Andrade <124772095+ramonsaguini@users.noreply.github.com>
2024-03-03 11:15:21 +0000

    Comp - Header - JSX - fixed absolut route

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyHeader from './components/Header';
 import MyFooter from './components/Footer';
 import { Candidate, Custom404, Home, JobSearch, JobResults, EmployerSearch, JobDetails } from './pages';
-import './App.css'
+import './App.css';
 import React from 'react';
 
-
 function App() {
-
   return (
-    <Router >
+    <Router>
       <MyHeader />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='candidate' element={<Candidate />} />
+        <Route path='/candidate' element={<Candidate />} />
         <Route path='/job-results' element={<JobResults />} />
-        <Route path='employer/search' element={<EmployerSearch />} />
-        <Route path='404' element={<Custom404 />} />
-        <Route path="/job/search" element={<JobSearch />} />
-        <Route path="/job-details/:id" element={<JobDetails />} />
+        <Route path='/employer/search' element={<EmployerSearch />} />
+        <Route path='/404' element={<Custom404 />} />
+        <Route path='/job/search' element={<JobSearch />} />
+        <Route path='/job-details/:id' element={<JobDetails />} />
       </Routes>
       <MyFooter />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
