38a35fde940d0f042db91f494c90268f35011982
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-03-03 18:09:44 +0000

    Fix lose focus bug in search controls

--- src/pages/JobSearch.jsx
+++ src/pages/JobSearch.jsx
@@ -74,15 +74,6 @@ function JobSearch() {
   //   }));
   // };
 
-  
-// Test a basic input field
-const [formTest, setFormTest] = useState(undefined);
-const handleTestChange = (event) => {
-const testValue = event.target.value;
-setFormTest(testValue);
-};
-
-
   // Handle Clear button
   const handleJobSearchClear = () => {
     setFormData({ query: '', location: '' });
@@ -151,61 +142,52 @@ setFormTest(testValue);
     setPage(page + delta)
   };
 
-  const JobSearchFilter = () => {
-    return (
-      <>
-        <form onSubmit={handleSearchFormSubmit} className="bg-white rounded-lg p-2 mr-2">
-          <div className="space-y-12">
-            <div className="border-b border-gray-900/10 pb-6">
-              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+  return (
+    <div className="container mx-auto">
 
+      <h1 className="text-3xl font-semibold leading-7 text-gray-900 sm:text-4xl mb-6">Job Search</h1>
 
-<div className="col-span-full">
-<label htmlFor="test" className="block text-sm font-medium leading-6 text-gray-900 text-left">Test</label>
-<input
-value={formTest}
-onChange={handleTestChange}
-id="test-id"
-name="test"
-type="text"
-placeholder="Test field"
-className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-required />
-</div>
+      <div className="flex flex-row sm:flex-col md:flex-row lg:flex-row">
+        <aside className="w-4/12 sm:w-full md:w-4/12">
 
-                <div className="col-span-full">
-                  <label htmlFor="query" className="block text-sm font-medium leading-6 text-gray-900 text-left">
-                    Job Title
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      value={formData.query}
-                      onChange={handleQueryChange}
-                      type="text"
-                      name="query"
-                      placeholder="eg Frontend Developer"
-                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      required />
+          <form onSubmit={handleSearchFormSubmit} className="bg-white rounded-lg p-2 mr-2">
+            <div className="space-y-12">
+              <div className="border-b border-gray-900/10 pb-6">
+                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+                  <div className="col-span-full">
+                    <label htmlFor="query" className="block text-sm font-medium leading-6 text-gray-900 text-left">
+                      Job Title
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        value={formData.query}
+                        onChange={handleQueryChange}
+                        type="text"
+                        name="query"
+                        placeholder="eg Frontend Developer"
+                        className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        required />
+                    </div>
                   </div>
-                </div>
 
-                <div className="col-span-full">
-                  <label htmlFor="location" className="block text-sm font-medium leading-6 text-gray-900 text-left">
-                    Location
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      value={formData.location}
-                      onChange={handleLocationChange}
-                      name="location"
-                      type="text"
-                      placeholder="City, Country"
-                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      required />
+                  <div className="col-span-full">
+                    <label htmlFor="location" className="block text-sm font-medium leading-6 text-gray-900 text-left">
+                      Location
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        value={formData.location}
+                        onChange={handleLocationChange}
+                        name="location"
+                        type="text"
+                        placeholder="City, Country"
+                        className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        required />
+                    </div>
                   </div>
-                </div>
 
-                {/* <div className="sm:col-span-3">
+                  {/* <div className="sm:col-span-3">
                 <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
                   Country
                 </label>
@@ -223,7 +205,7 @@ required />
                 </div>
               </div> */}
 
-                {/* <div className="col-span-full">
+                  {/* <div className="col-span-full">
                 <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
                   Street address
                 </label>
@@ -238,37 +220,25 @@ required />
                 </div>
               </div> */}
 
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="mt-6 flex justify-start gap-x-6">
-            <button type="button"
-              onClick={handleJobSearchClear}
-              className="rounded-md px-3 py-2 text-sm font-semibold bg-gray-200 text-gray-600 hover:bg-gray-300 focus:ring-gray-400">
-              Clear
-            </button>
-            <button
-              type="submit"
-              disabled={isSearching}
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              Find Jobs
-            </button>
-          </div>
-        </form>
+            <div className="mt-6 flex justify-start gap-x-6">
+              <button type="button"
+                onClick={handleJobSearchClear}
+                className="rounded-md px-3 py-2 text-sm font-semibold bg-gray-200 text-gray-600 hover:bg-gray-300 focus:ring-gray-400">
+                Clear
+              </button>
+              <button
+                type="submit"
+                disabled={isSearching}
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Find Jobs
+              </button>
+            </div>
+          </form>
 
-      </>
-    )
-  };
-
-  return (
-    <div className="container mx-auto">
-
-      <h1 className="text-3xl font-semibold leading-7 text-gray-900 sm:text-4xl mb-6">Job Search</h1>
-
-      <div className="flex flex-row sm:flex-col md:flex-row lg:flex-row">
-        <aside className="w-4/12 sm:w-full md:w-4/12">
-          <JobSearchFilter />
         </aside>
 
         <section className="w-8/12 sm:w-full">
