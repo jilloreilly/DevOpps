ca4e25f0ed3add123e3a913b664ea612e16cae75
jillo <jill.l.oreilly@gmail.com>
2024-03-04 22:39:57 +0000

    Remove unused imports. Remove Favourites function to JobDetails.jsx

--- src/components/JobList/index.jsx
+++ src/components/JobList/index.jsx
@@ -1,18 +1,12 @@
-// import { Link } from "react-router-dom";
-// import { useState } from 'react';
-
-export default function JobList(props) {
+export default function JobList({data}) {
   
-  const jobs = props.data;
-
-  console.log(jobs);
-
-
-  
+  const jobs = data;
+    
   return (
     <>
     
       <div key={jobs.id} className="flex flex-col w-full mb-3 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-3">
+      {/* <div className="" onClick={addToFavourites}><i className="fa-regular fa-heart"></i></div> */}
         <div className="flex flex-col">
           <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{jobs.title}</h2>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"><i className="fa-regular fa-building"></i> <strong>Company:</strong> {jobs.company}</p>
