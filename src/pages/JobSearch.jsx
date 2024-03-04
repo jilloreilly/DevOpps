5c15432a9223d00bf378caa9e8d61d50ced579fe
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-03-04 09:37:32 +0000

    Add Employment Types component

--- src/pages/JobSearch.jsx
+++ src/pages/JobSearch.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import getJobs from "../services/getJobs.js";
 import JobSearchResults from "../components/JobSearch/Results.jsx";
+import EmploymentTypes from '../components/JobSearch/EmploymentTypes.jsx';
 
 function JobSearch() {
 
@@ -17,6 +18,7 @@ function JobSearch() {
   const [formData, setFormData] = useState({
     query: '',
     location: '',
+    employmentTypes: '',
   });
 
   // Get querystring values and set form data on first render
@@ -97,6 +99,7 @@ function JobSearch() {
       const response = await getJobs({
         query: query,
         location: location,
+        employmentTypes: formData.employmentTypes,
         index: page
       });
 
@@ -158,7 +161,6 @@ function JobSearch() {
                         <option value="Back End Developer">Back End Developer</option>
                         <option value="Full Stack Developer">Full Stack Developer</option>
                       </select>
-
                     </div>
                   </div>
 
@@ -178,38 +180,59 @@ function JobSearch() {
                     </div>
                   </div>
 
-                  {/* <div className="sm:col-span-3">
-                <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
-                  Country
-                </label>
-                <div className="mt-2">
-                  <select
-                    id="country"
-                    name="country"
-                    autoComplete="country-name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                  >
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
-                  </select>
-                </div>
-              </div> */}
-
                   {/* <div className="col-span-full">
-                <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
-                  Street address
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="street-address"
-                    id="street-address"
-                    autoComplete="street-address"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div> */}
+                    <label htmlFor="Distance" className="block text-sm font-medium leading-6 text-gray-900 text-left">
+                      Distance (km)
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        value={formData.distance}
+                        onChange={handleInputChange}
+                        name="distance"
+                        type="text"
+                        placeholder="Distance from location (optional)"
+                        className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-span-full">
+                    <div className="mt-2">
+                      <label htmlFor="remoteOnly" className="text-sm font-medium leading-6 text-gray-900 text-left">
+                        Remote Only
+                      </label>
+                      <input
+                        value={formData.remoteOnly}
+                        onChange={handleInputChange}
+                        name="remoteOnly"
+                        type="checkbox"
+                        placeholder="Distance from location (optional)"
+                        className="rounded-md border-0 py-1.5 ml-3 px-2 text-gray-900 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-span-full">
+                    <label htmlFor="posted" className="block text-sm font-medium leading-6 text-gray-900 text-left">
+                      Posted
+                    </label>
+                    <div className="mt-2">
+                      <select
+                        value={formData.posted}
+                        onChange={handleInputChange}
+                        name="posted"
+                        className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      >
+                        <option value="">Show jobs posted...</option>
+                        <option value="today">Today</option>
+                        <option value="3days">Last 3 days</option>
+                        <option value="week">One week</option>
+                        <option value="month">One month</option>
+                      </select>
+                    </div>
+                  </div> */}
+
+                  <EmploymentTypes value={formData.employmentTypes || ''} onChange={handleInputChange} />                  
 
                 </div>
               </div>
