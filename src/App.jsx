cc40724cf4a7c96bdb85c8d36c959ba55477fac2
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-03-04 21:02:40 +0000

    added a function to the app that should render the candidates json file to the local storage when the app loads

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,47 @@ import CandidateProfileForm from './components/CandidateProfileForm';
 import { Custom404, Home, JobSearch, EmployerSearch, JobDetails, Candidate } from './pages';
 import './App.css'
 import React from 'react';
-import userData from '../candidates.json'
+
 
 function App() {
+
+  const [candidateArray, setCandidateArray] = useState([]);
+
+  useEffect(() => {
+    // Function to load data from JSON file
+    const loadDataFromJSON = async () => {
+      fetch('../../candidates.json')
+        .then(response => {
+          return response.json()
+        }).then(data => {
+          console.log(data)
+          setCandidateArray(data)
+        })
+    } 
+
+    loadDataFromJSON();
+
+    const localStorageData = localStorage.getItem('candidateData');
+    if (!localStorageData) {
+      localStorage.setItem('candidateData', JSON.stringify([]));
+    }
+  }, []);
+
+  const saveArrayToLocal = () => {
+    localStorage.setItem('candidateData', JSON.stringify(candidateArray));
+    console.log('Array saved to localStorage');
+  };
+
+  saveArrayToLocal();
+
+
   return (
     <Router>
       <MyHeader />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/candidate/create-profile' element={<CandidateProfileForm />} />
-        <Route path="/candidate/profile/:id" element={<Candidate userData={userData} />} />
+        <Route path="/candidate/profile/:id" element={<Candidate userData={candidateArray} />} />
         <Route path='/job-results' element={<JobSearch />} />
         <Route path='/employer/search' element={<EmployerSearch />} />
         <Route path='*' element={<Custom404 />} />
