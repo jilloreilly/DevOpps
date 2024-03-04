417731407262b7566ba57b635ef5e037c3318fba
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-03-04 23:15:08 +0000

    noticed the success and error messages were missing the colours, this is due to the way the tailwind classes are being added, so I just added them to the index.css file

--- src/components/CandidateGitHubProfile/index.jsx
+++ src/components/CandidateGitHubProfile/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import './index.css'
 
 function CandidateGitHubProfile({ onGitHubInputChange, onGitHubDetailsChange }) {
   
@@ -84,7 +85,7 @@ function CandidateGitHubProfile({ onGitHubInputChange, onGitHubDetailsChange }) 
               />
               <button id ="fetch-git-hub" onClick={handleGitHubClick}
                 type="submit"
-                className="ms-4 flex-none rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="ms-4 flex-none rounded-md  bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Link GitHub
               </button>
