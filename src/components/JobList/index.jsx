4f53796916c30112824563303b7b7d31d4f84619
jillo <jill.l.oreilly@gmail.com>
2024-03-05 12:32:35 +0000

    Reduce font size of job data

--- src/components/JobList/index.jsx
+++ src/components/JobList/index.jsx
@@ -10,7 +10,7 @@ export default function JobList({data}) {
           <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{jobs.title}</h2>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"><i className="fa-regular fa-building"></i> <strong>Company:</strong> {jobs.company}</p>
         </div>
-        <div className="flex flex-row w-full justify-between leading-normal">
+        <div className="flex flex-row w-full justify-between text-sm leading-normal">
           <p><i className="fa-regular fa-compass"></i> <strong>Location:</strong> {jobs.location}</p>
           <p><i className="fa-regular fa-clock"></i><strong>Employment type:</strong> {jobs.employmentType}</p>
           <p><i className="fa-regular fa-calendar"></i> <strong>Date posted:</strong> {jobs.datePosted}</p>
