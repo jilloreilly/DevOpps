d0a332d08e66dc6615301d62b0006a97063d9747
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-03-05 19:31:16 +0000

    updated the search flow on mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,42 +38,44 @@ function Home() {
     <>
       <div className="h-full flex items-center justify-center xl:justify-start lg:justify-center md:justify-center sm:justify-center hero-background relative">
         <div className="absolute  inset-0 bg-white opacity-20"></div>
-        <div className="w-1/2 xl:p-20 lg:p-5 flex flex-col relative z-10">
-          <h1 className="text-5xl font-medium text-white mb-8">Welcome to devOpps</h1>
-          <h3 className="text-2xl font-light text-white mb-14">From job seekers to recruiters, devOpps offers a comprehensive platform for all your tech hiring needs, making job searches and candidate sourcing hassle-free.</h3>
-          <div className=' bg-white rounded-lg p-5'>
-            <h2 className='text-gray-800 pb-3 mx-2'>Search available positions:</h2>
-            <form onSubmit={(e) => { e.preventDefault(); handleSearch(); }} className="flex flex-row items-center">
-              <Dropdown>
-                <DropdownTrigger>
-                  <Button variant="bordered" className="w-full bg-gray-200 text-gray-600 hover:text-gray-300 hover:bg-gray-400 h-12 mx-2 rounded-medium border border-gray-400">
-                    {selectedOption || 'Select a job type'}
-                  </Button>
-                </DropdownTrigger>
-                <DropdownMenu aria-label="Static Actions">
-                  <DropdownItem key="Software Developer" onClick={() => setSelectedOption('Software Developer')}>
-                    Software Developer
-                  </DropdownItem>
-                  <DropdownItem key="Front End Developer" onClick={() => setSelectedOption('Front End Developer')}>
-                    Front End Developer
-                  </DropdownItem>
-                  <DropdownItem key="Back End Developer" onClick={() => setSelectedOption('Back End Developer')}>
-                    Back End Developer
-                  </DropdownItem>
-                  <DropdownItem key="Full Stack Developer" onClick={() => setSelectedOption('Full Stack Developer')}>
-                    Full Stack Developer
-                  </DropdownItem>
-                  <DropdownItem key="Full Stack Developer" onClick={() => setSelectedOption('DevOps Engineer')}>
-                    DevOps Engineer
-                  </DropdownItem>
-                  <DropdownItem key="Full Stack Developer" onClick={() => setSelectedOption('Cloud Architect')}>
-                    Cloud Architect
-                  </DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-              <Input type="location" label="Enter your location" value={location} onChange={(e) => setLocation(e.target.value)} className="text-gray-600 hover:text-gray-300 rounded-medium h-12 mx-1 border border-gray-400 w-full" />
-              <Button type="submit" className="bg-indigo-600 hover:bg-indigo-400 shadow-lg text-white rounded-medium h-12 w-full border border-indigo-200 mx-2">Search</Button>
-            </form>
+        <div className="container max-w-[1280px] w-full px-6 py-10 xl:py-20 flex flex-col relative z-10 mx-auto">
+          <div className="lg:w-3/4 xl:w-1/2">
+            <h1 className="text-5xl font-medium text-white mb-6">Welcome to devOpps</h1>
+            <h3 className="text-2xl font-light text-white mb-10">From job seekers to recruiters, devOpps offers a comprehensive platform for all your tech hiring needs, making job searches and candidate sourcing hassle-free.</h3>
+            <div className=' bg-white rounded-lg p-5'>
+              <h2 className='text-gray-800 pb-3 mx-2'>Search available positions:</h2>
+              <form onSubmit={(e) => { e.preventDefault(); handleSearch(); }} className="flex flex-row items-center">
+                <Dropdown>
+                  <DropdownTrigger>
+                    <Button variant="bordered" className="w-full bg-gray-200 text-gray-600 hover:text-gray-300 hover:bg-gray-400 h-12 mx-2 rounded-medium border border-gray-400">
+                      {selectedOption || 'Select a job type'}
+                    </Button>
+                  </DropdownTrigger>
+                  <DropdownMenu aria-label="Static Actions">
+                    <DropdownItem key="Software Developer" onClick={() => setSelectedOption('Software Developer')}>
+                      Software Developer
+                    </DropdownItem>
+                    <DropdownItem key="Front End Developer" onClick={() => setSelectedOption('Front End Developer')}>
+                      Front End Developer
+                    </DropdownItem>
+                    <DropdownItem key="Back End Developer" onClick={() => setSelectedOption('Back End Developer')}>
+                      Back End Developer
+                    </DropdownItem>
+                    <DropdownItem key="Full Stack Developer" onClick={() => setSelectedOption('Full Stack Developer')}>
+                      Full Stack Developer
+                    </DropdownItem>
+                    <DropdownItem key="Full Stack Developer" onClick={() => setSelectedOption('DevOps Engineer')}>
+                      DevOps Engineer
+                    </DropdownItem>
+                    <DropdownItem key="Full Stack Developer" onClick={() => setSelectedOption('Cloud Architect')}>
+                      Cloud Architect
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+                <Input type="location" label="Enter your location" value={location} onChange={(e) => setLocation(e.target.value)} className="text-gray-600 hover:text-gray-300 rounded-medium h-12 mx-1 border border-gray-400 w-full" />
+                <Button type="submit" className="hover:bg-gray-400 shadow-md text-gray-800 font-bold rounded-medium h-12 w-full border border-gray-400 mx-2">Search</Button>
+              </form>
+            </div>
           </div>
 
           {errorMessage && <p className="text-white">{errorMessage}</p>}
@@ -107,14 +109,14 @@ function Home() {
 <div className="container max-w-[1280px] px-6 mx-auto">
 {/* Popular cities text */}
         <div className='text-center flex items-center justify-center flex-col'>
-          <h2 className='lg:w-1/2 mb-4 text-3xl font-bold'>Popular cities</h2>
-          <p className='lg:w-1/2 mb-5'>Our popular cities section enables candidates to explore job openings in their preferred locations. Click on a city to initiate a targeted job search and uncover exciting career prospects near you.</p>
+          <h2 className='lg:w-3/4 xl:w-1/2 mb-4 text-3xl font-bold'>Popular cities</h2>
+          <p className='lg:w-3/4  xl:w-1/2 mb-5'>Our popular cities section enables candidates to explore job openings in their preferred locations. Click on a city to initiate a targeted job search and uncover exciting career prospects near you.</p>
         </div>
 </div>
 
 
 {/* Popular cities grid */}
-<div className="container max-w-[1280px] grid lg:grid-cols-3 md:grid-cols-3 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-6 gap-8 pb-20 my-5 md:my-24 px-6  mx-auto">
+<div className="container max-w-[1280px] grid lg:grid-cols-3 md:grid-cols-2 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-6 gap-8 my-5 md:my-24 px-6  mx-auto">
           <a className="relative london-box rounded-lg" onClick={() => handleCityClick('London')}><div>
             <div className="absolute inset-0 bg-white opacity-10 hover:opacity-0"></div>
             <h2 className="absolute bottom-0 right-0 mr-3 mb-3 text-white text-2xl">London</h2>
