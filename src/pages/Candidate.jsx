3b3d60253eda77bd2644e819b6eb440ee9981dba
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-03-05 10:05:47 +0000

    updated candidate page with some styling

--- src/pages/Candidate.jsx
+++ src/pages/Candidate.jsx
@@ -64,30 +64,44 @@ function Candidate() {
       return <div>User not found</div>;
     }
   
-    return (
-      <div className="max-w-[1280px] container my-10 mx-auto px-6">
-        <img className="rounded-full my-5" src={user.gitHubAvatar}></img>
-        <h2 className="text-3xl font-semibold leading-7 text-gray-900 sm:text-4xl mb-4">{user.name}</h2>
-        <p>Email: {user.email}</p>
-        <p>City: {user.city}</p>
-        <p>Experience: {user.experience}</p>
-        <p>Role: {user.role}</p>
-        <p>Workplace Preference: {user.workplace}</p>
-        <button className="mt-4 flex-none rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500" onClick={findJobs}>Find matching jobs</button>
+  return (
+      <>
+    <div className=" bg-indigo-500  py-24 mx-auto ">
+    <div className ="container mx-auto max-w-[1280px] px-6">
+       <h1 className=" text-3xl font-semibold leading-7 text-white sm:text-4xl">{user.name} - {user.role}</h1>
+       
+    </div>
+   
+   </div>
+      <div className="max-w-[1280px] container mt-12 mx-auto px-6 flex flex-col md:flex-row lg:flex-row gap-12 ">
 
-        <h2 className="mt-3 text-2xl font-semibold">Skills:</h2>
-        <ul className="mt-5 flex flex-wrap flex-row items-center justify-start gap-4">
-        {user.technology.map(tech => (
-          <div className="text-indigo-700 max-w-fit inline-flex items-center justify-center box-border whitespace-nowrap border-medium p-3 h-7 rounded-full border-indigo-100 bg-indigo-100 hover:bg-indigo-300 hover:border-indigo-300 relative select-none text-medium transition-colors-opacity before:transition-width motion-reduce:transition-none" key={tech}>{tech}</div>
-        ))}
-      </ul>
-        <h2 className="mt-3 text-2xl font-semibold">GitHub Repos:</h2>
-        <ul className="mt-5 flex flex-wrap flex-row items-center justify-start gap-3">
-         {repos.map(repo => (
-          <a href={repo.html_url} target="_blank" className="bg-blue-100 hover:bg-blue-200 text-blue-800 text-xs font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 inline-flex items-center justify-center" key={repo.id}>{repo.name} </a>
-        ))}
-      </ul>
+       <div className="w-full md:w-5/12"> <img className="rounded-full mb-6" src={user.gitHubAvatar}></img></div>
+
+
+       <div className='w-full md:w-7/12'>
+       <h2 className="my-6 text-2xl font-semibold">Details:</h2>
+          <p>Email: {user.email}</p>
+          <p>City: {user.city}</p>
+          <p>Experience: {user.experience}</p>
+          <p>Workplace Preference: {user.workplace}</p>
+          <button className="mt-6 flex-none rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500" onClick={findJobs}>Find matching jobs</button>
+  
+          <h2 className="mt-6 text-2xl font-semibold">Skills:</h2>
+          <ul className="mt-6 flex flex-wrap flex-row items-center justify-start gap-4">
+          {user.technology.map(tech => (
+            <div className="text-indigo-700 max-w-fit inline-flex items-center justify-center box-border whitespace-nowrap border-medium p-3 h-7 rounded-full border-indigo-100 bg-indigo-100 hover:bg-indigo-300 hover:border-indigo-300 relative select-none text-medium transition-colors-opacity before:transition-width motion-reduce:transition-none" key={tech}>{tech}</div>
+          ))}
+        </ul>
+          <h3 className="mt-6 text-2xl font-semibold">GitHub Repos:</h3>
+          <ul className="mt-6 flex flex-wrap flex-row items-center justify-start gap-3">
+           {repos.map(repo => (
+            <a href={repo.html_url} target="_blank" className="bg-blue-100 hover:bg-blue-200 text-blue-800 text-xs font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 inline-flex items-center justify-center" key={repo.id}>{repo.name} </a>
+          ))}
+          </ul>
+       </div>
+        
       </div>
+      </>
     );
   }
 
