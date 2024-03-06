3ff7281f778d951efca2357f39ea99807ba51b98
jillo <jill.l.oreilly@gmail.com>
2024-03-06 12:14:12 +0000

    Add flex-wrap and gap-4 classes to containing divs of job spec and jobProviders

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -76,13 +76,13 @@ function JobDetails(props) {
         </div>
         <div className="favourite" onClick={addToFavourites}><i className={isChecked ? "fa-solid fa-heart text-indigo-500" : "fa-regular fa-heart text-indigo-500"}></i></div>
       </div>
-      <div className='flex flex-row sm:flex-col md:flex-row lg:flex-row justify-evenly py-3 border-t-1 border-b-1'>
+      <div className='flex flex-row sm:flex-col md:flex-row lg:flex-row justify-evenly flex-wrap gap-4 py-3 border-t-1 border-b-1'>
         <p><i className="fa-regular fa-compass text-indigo-500"></i> <strong>Location:</strong> {data.location}</p>
         <p><i className="fa-regular fa-clock text-indigo-500"></i> <strong>Employment Type:</strong> {data.employmentType}</p>
         <p><i className="fa-regular fa-calendar text-indigo-500"></i> Date posted: {data.datePosted}</p>
         <p><i className="fa-regular fa-money-bill-1 text-indigo-500"></i> <strong>Salary range:</strong> {data.salaryRange == "" ?  "Competitive" : data.salaryRange}</p>
       </div>
-      <div className='flex flex-row sm:flex-col md:flex-row lg:flex-row justify-evenly py-3 border-b-1'>
+      <div className='flex flex-row sm:flex-col md:flex-row lg:flex-row justify-evenly flex-wrap gap-4 py-3 border-b-1'>
         <h3 className="font-semibold">Apply here</h3>
         {data.jobProviders.map(provider => (
           <p key={provider.jobProvider}><a href={provider.url} target='_blank' className='text-sm hover:text-indigo-400'>{provider.jobProvider} <i className="fa-regular fa-share-from-square text-indigo-500"></i></a></p>
