2f741458cc21746fe6019fa0d960fe37db48d991
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-03-06 18:54:28 +0000

    fixed minor responive issues in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -12,16 +12,16 @@ const MyFooter = () => {
     return (
         <footer className="bg-blue-100 text-black py-10 mt-20 relative bottom-0 left-0 w-full">
             {/* Top section for logo and links within website */}
-            <div className="container mx-auto flex justify-around lg:flex-row md:flex-row items-center sm:flex-col p-10 md:p-5 sm:p-2">
+            <div className="container max-w-[1280px] mx-auto flex flex-col justify-around lg:flex-row md:flex-row items-center sm:flex-col p-10 md:p-5 sm:p-2">
                 {/* Left section for logo +/- text */}
-                <div className="w-1/3 flex flex-row items-center justify-center pb-2">
+                <div className="w-full lg:w-1/3 flex flex-row items-center justify-center pb-2">
                     <p className='p-5 md:p-1 text-center text-large md:text-base'>devOpps simplifies the hiring process by enabling developers to find their ideal job matches while assisting employers in discovering top talent. </p>
                 </div>
                 <div className="w-full lg:w-1/3 flex flex-col items-center justify-center pb-2">
                     <Link to='/' onClick={scrollToTop}><img src='/images/logo.png'/></Link>
                 </div>
                 {/* Right section for quick links */}
-                <div className='flex flex-col items-center w-1/3 pt-2'>
+                <div className='flex flex-col items-center w-full lg:w-1/3 pt-2'>
                     <h3 className='text-lg md:text-base p-3 sm:p-1'>Quick Links</h3>
                     <Link to="/employer/search" className="text-base md:text-sm text-black p-2 sm:p-1 hover:text-indigo-400">Employers</Link>
                     <Link to="/candidate/create-profile" className="text-base md:text-sm text-black p-2 sm:p-1 hover:text-indigo-400">Candidates</Link>
