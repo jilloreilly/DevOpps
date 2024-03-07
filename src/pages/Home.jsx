15ff7d6097ac58caeaa4027d5f7ab317b08b40b0
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-03-07 09:47:39 +0000

    removed : from end of latest news title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -152,7 +152,7 @@ function Home() {
       
       {/* tech header */}
       <div className='text-center flex items-center justify-center flex-col'>
-          <h2 className='lg:w-1/2 mb-10 font-bold mt-5 text-3xl'>Latest Tech News:</h2>
+          <h2 className='lg:w-1/2 mb-10 font-bold mt-5 text-3xl'>Latest Tech News</h2>
         </div>
 
         <div className="container max-w-[1280px] px-6 mx-auto">
